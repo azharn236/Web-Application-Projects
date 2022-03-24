dba155dea656240c353b7f1db14808d6cbb93d0c
Azhar N <azharn236@gmail.com>
2022-03-24 16:02:12 +0530

    added

---
+++ travelprojects/travelproject1/travelapp/views.py
@@ -0,0 +1,27 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from.models import Place
+from.models import Place2
+# Create your views here.
+
+
+def demo(request):
+    obj = Place.objects.all()
+    obj1 = Place2.objects.all()
+    return render(request, 'index.html', {'result': obj, 'result1': obj1})
+
+
+#
+# def addition(request):
+#     x = int(request.GET['num1'])
+#     y = int(request.GET['num2'])
+#     res = x+y
+#     return render(request, 'result.html', {'result': res})
+
+# def about(request):
+#     return render(request, 'about.html')
+#
+#
+# def contact(request):
+#     return HttpResponse('Hello, I am Contact')
